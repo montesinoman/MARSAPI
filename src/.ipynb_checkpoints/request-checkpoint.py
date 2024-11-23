b77e83d2c823d35c63fa6ae44b85e7dc8884eee4
Manuel Montesino San Martin <mmontesinosanmartin@gmail.com>
2024-11-23 18:22:41 +0100

    companies

---
+++ src/.ipynb_checkpoints/request-checkpoint.py
@@ -0,0 +1,76 @@
+###############################################################################
+# DEPENDENCIES
+###############################################################################
+
+import subprocess
+import json
+import pandas
+
+###############################################################################
+# CONSTANTS
+###############################################################################
+
+class URLIB:
+    # HEALTH block
+    HEALTH = 'https://methanedata.unep.org/api/healthcheck'
+    # COUNTRY block
+    COUNTRIES = 'https://methanedata.unep.org/api/countries'
+    COUNTRYSTATS = 'https://methanedata.unep.org/api/countries/%s/keystats'
+    COUNTRYFACTS = 'https://methanedata.unep.org/api/factsheets/country/%s/year/%s'
+    # PLUME block
+    PLUMES = 'https://methanedata.unep.org/api/plumes'
+    PLUMEGEOJSON = 'https://methanedata.unep.org/api/plumes/geojson'
+    PLUMECENTROID ='https://methanedata.unep.org/api/plumes/centroids/geojson'
+    PLUMEIMGCOORD = 'https://methanedata.unep.org/api/plumes/image-coordinates'
+    PLUMESAT = 'https://methanedata.unep.org/api/plumes/satellites'
+    PLUMESECTOR = 'https://methanedata.unep.org/api/sector'
+    PLUMESOURCE = 'https://methanedata.unep.org/api/plumes/sources'
+    # COMPANY block
+    COMPANY = 'https://methanedata.unep.org/api/companies'
+    COMPANYFACT = 'https://methanedata.unep.org/api/factsheets/company/%s/year/%s'
+
+###############################################################################
+# FUNCTIONS
+###############################################################################
+
+def _request(url):
+    ''' Sends the request and captures the response
+    :param url: str. URL for the request
+    :returns: str. Response
+    '''
+    # Send the request
+    response = subprocess.run(['curl', url],
+                              capture_output=True,
+                              text=True)
+    # Check all good
+    if response.returncode == 0:
+        # Get the response 
+        out =  response.stdout
+    # Otherwise
+    else:
+        # Warn
+        sys.exit(f"Error: {response.returncode} with {url}")
+    # Return the response
+    return out
+
+def _healthcheck():
+    ''' Checks that API is running
+    '''
+    # Check the health of the API
+    vitals = _request(URLIB.HEALTH)
+    # All goo if response is ''
+    ishealthy = ('MethaneData public API' == vitals)
+    # Retrun
+    return ishealthy
+
+def _json_response_to_df(response):
+    ''' Convert a request response to data frame
+    :param response: str. Request reponse.
+    '''
+    # Load the JSON
+    json_response = json.loads(response)
+    # Coercing to DataFrame
+    df_response = pandas.DataFrame(json_response)
+    # Return the dataframe
+    return df_response
+    
---
+++ src/company.py
@@ -0,0 +1,51 @@
+###############################################################################
+# DEPENDENCIES
+###############################################################################
+
+import request
+
+###############################################################################
+# CONSTANTS
+###############################################################################
+
+###############################################################################
+# FUNCTIONS
+###############################################################################
+
+def list_all():
+    ''' Lists all the OGMP companies
+    :returns: DataFrame. Company IDs and names.
+    '''
+    # Get the list
+    companies = request._request(request.URLIB.COMPANY)
+    # Companies as data frame
+    companies = request._json_response_to_df(companies)
+    # Return the list
+    return companies
+
+def find_id(companyname):
+    ''' Tries to find the company ID
+    :param companyname: str. Name of the company.
+    '''
+    # List all companies
+    all_companies = list_all()
+    # Find the company name
+    this_company = all_companies[
+        all_companies['name'].str.contains(companyname, case=False, na=False)]
+    # Return the ID
+    return this_company['idCompany'].iloc[0]
+
+def get_info(companyid, year):
+    ''' Get information of a company for a given year
+    :param companyid: str. IMEO company ID.
+    :param year: int. Information on that year.
+    :returns:
+    '''
+    # Build URL
+    this_url = request.URLIB.COMPANYFACT % (companyid, str(year))
+    # Request the information
+    company_facts = request._request(this_url)
+    # Turn into a data frame
+    company_facts = request._json_response_to_df(company_facts)
+    # Return
+    return company_facts
